a96205ef8c6a584c9c39f73c2c4e4ba2784e00b8
Eric Anderson <elanderson@gmail.com>
2017-03-01 13:03:22 +0000

    Add contact class and context

---
+++ Sqlite/Sqlite/Data/ContactsContext.cs
@@ -0,0 +1,25 @@
+﻿using EfSqlite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfSqlite.Data
+{
+    public sealed class ContactsContext : DbContext
+    {
+        private static bool _created;
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public ContactsContext()
+        {
+
+        }
+
+        public ContactsContext(DbContextOptions<ContactsContext> options)
+            : base(options)
+        {
+            if (_created) return;
+            Database.Migrate();
+            _created = true;
+        }
+    }
+}
---
+++ Sqlite/Sqlite/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfSqlite.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
